1fa4693f91a4eef9c90c31ec855b9ee05627a664
animated123 <60016764+animated123@users.noreply.github.com>
2024-05-28 06:17:28 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+#Code for user_input 
+
+name = input("Please enter your name: ")
+
+age = input("Please enter your age: ")
+
+location = input("Please enter your location: ")
+
+print(f"Hi {name}, I see you are {age} years old and live in {location}. Thanks {name}")
